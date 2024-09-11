ac3f2a2d33097abbae8dc7997341dcb31b300737
Nexusrex <ahmedtahoun55@gmail.com>
2024-09-11 19:43:51 +0300

    feat: Added exit button

--- UI/Title Screen/TitleScreen.cs
+++ UI/Title Screen/TitleScreen.cs
@@ -6,12 +6,15 @@ public partial class TitleScreen : Node2D
 	[Export]
 	private AudioStream _startSFX;
 
+	[ExportGroup("Buttons")]
 	[Export]
 	private Button _startButton;
+	[Export]
+	private Button _exitButton;
 
+	[ExportGroup("Transition Settings")]
 	[Export]
 	private PackedScene _transitionScene;
-
 	[Export]
 	private SceneNames _nextScene;
 
@@ -22,6 +25,7 @@ public partial class TitleScreen : Node2D
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		_startButton.GrabFocus();
 		_startButton.Pressed += ButtonPressed;
+		_exitButton.Pressed += () => { GetTree().Quit(); };
 	}
 
 	private void ButtonPressed()
